94c2c4e7085c9102342768d65249750cc37e95ba
zamandalee <a@zalee.me>
2021-12-06 16:23:20 -0500

    Add props for popup component and classnames package

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { getCounter } from "./utils/functions";
+
+
+const domains = [] // TODO: fetch domains from backend!!
 
 ReactDOM.render(
-  <App />,
+  <App domains={domains} changePassword={domain => getCounter(domain, true)}/>,
   document.getElementById('root')
 );
 
