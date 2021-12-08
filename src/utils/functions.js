fcc795c32b3d8956d9e97ccdd339a0901ddb9ead
Mason Zhang <masonzhang21@gmail.com>
2021-12-07 23:58:24 -0500

    Favicon getter

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -84,7 +84,15 @@ const sum = function (arr) {
 }
 
 
-let imgUrl = "https://" + props.site + "/favicon.ico";
+import React, { useState } from "react";
+
+/**
+ * Site favicon + abbreviated URL
+ * @param {*} props 
+ */
+export default function Site(props) {
+  const [imageExists, setImageExists] = useState(false);
+  let imgUrl = "https://" + props.site + "/favicon.ico";
 
   //tests if a favicon exists at the above url
   let image = document.createElement("img");
@@ -95,3 +103,18 @@ let imgUrl = "https://" + props.site + "/favicon.ico";
   image.onerror = () => {
     setImageExists(false);
   };
+
+  return (
+    <div style={{ height: "100%", display: "flex", alignItems: "center"}}>
+      <img
+        className="mx-2"
+        src={imageExists ? imgUrl : require("../Resources/missingImage.png")}
+        alt=""
+        style={{
+          height: "100%"
+        }}
+      ></img>
+      {props.site}
+    </div>
+  );
+}
