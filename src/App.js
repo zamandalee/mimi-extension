f6454406128331bec67c1df736b7c72ac1025b9c
zamandalee <a@zalee.me>
2021-12-08 00:34:41 -0500

    Start adding frontend tabs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Collapsible from 'react-collapsible';
+var classNames = require('classnames');
 
 const IN_PROGRESS = 'IN_PROGRESS'
 const CHANGED = 'CHANGED'
@@ -15,7 +17,11 @@ class App extends React.Component {
 	componentDidMount() {
 		const formattedDomains = {}
 		this.props.domains.forEach(domain => { formattedDomains[domain] = UNCHANGED })
-		this.setState({ mimiDomains: formattedDomains })
+		this.setState({ mimiDomains: formattedDomains, activeTab: 'vault' })
+	}
+
+	handleTabClick(activeTab) {
+		this.setState({ activeTab })
 	}
 
 	changePassword(domain) {
@@ -32,9 +38,9 @@ class App extends React.Component {
 	}
 
 	render() {
-		// Domain list
-		const { mimiDomains } = this.state
+		const { mimiDomains, activeTab } = this.state
 
+		// Domain list
 		let domains = Object.keys(mimiDomains).map(domain => {
 			// Button to change password for this domain
 			let pwMessage = <div className="change-pw" onClick={this.changePassword.bind(this, domain)}>Change Password</div>
@@ -52,28 +58,85 @@ class App extends React.Component {
 				</div>
 			);
 		});
-
-		// If no domains
+		// Empty domain list
 		if (Object.keys(mimiDomains).length === 0) {
 			domains = <div className="no-domains">No passwords saved with MiMi yet.</div>
 		}
+		// FAQs
+		const faqs = (
+			<div className="faqs">
+				<Collapsible trigger="How do I use MiMi?"></Collapsible>
+				<Collapsible trigger="Is MiMi secure?"></Collapsible>
+			</div>
+		)
+
+		// Active tab logic
+		const activeContent = activeTab === 'vault' ? (<div className="domains">{domains}</div>) : faqs
 
 		return (
 			<div className="popup">
-				<div className="domains-index">
-					{domains}
+				<div className="tabs">
+					<div className={classNames({ 'active': activeTab === 'vault' })}
+						onClick={this.handleTabClick.bind(this, 'vault')}>
+						Vault
+					</div>
+					<div className={classNames({ 'active': activeTab === 'faq' })}
+						onClick={this.handleTabClick.bind(this, 'faq')}>
+						FAQs
+					</div>
 				</div>
-				<div>
-					<div className="popup-title">How to Use MiMi:</div>
-					<ol>
-						<li>Click into a password input field</li>
-						<li>Fill in your MiMi master password</li>
-						<li>Hit command+shift+p to transform your master password</li>
-					</ol>
-				</div>
+
+				{activeContent}
 			</div>
 		);
 	}
 }
 
 export default App;
+
+
+/*
+*
+
+<div className="popup-title">How to Use MiMi:</div>
+					<ol>
+						<li>Click into a password input field</li>
+						<li>Fill in your MiMi master password</li>
+						<li>Hit command+shift+p to transform your master password</li>
+					</ol>
+				</div>
+
+
+
+import React, { useState } from "react";
+
+/**
+export default function Site(props) {
+	const [imageExists, setImageExists] = useState(false);
+	let imgUrl = "https://" + props.site + "/favicon.ico";
+
+	//tests if a favicon exists at the above url
+	let image = document.createElement("img");
+	image.src = imgUrl;
+	image.onload = () => {
+		setImageExists(true);
+	};
+	image.onerror = () => {
+		setImageExists(false);
+	};
+
+	return (
+		<div style={{ height: "100%", display: "flex", alignItems: "center" }}>
+			<img
+				className="mx-2"
+				src={imageExists ? imgUrl : require("../Resources/missingImage.png")}
+				alt=""
+				style={{
+					height: "100%"
+				}}
+			></img>
+			{props.site}
+		</div>
+	);
+}
+ */
--- src/content-script.js
+++ src/content-script.js
@@ -26,4 +26,3 @@ function doStuff() {
 	passField.value = mimi;
 }
 
-doStuff();
